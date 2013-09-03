053f04563056eb43d601e93443f7e525594f115e
NovemberFoxtrot <november@fox.trot>
2013-09-03 02:02:04 -0700

    well well well look what I found

---
+++ smash.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Gear struct {
+	chainring float64
+	cog       float64
+}
+
+func NewGear(chainring, cog float64) Gear {
+	return Gear{chainring, cog}
+}
+
+func (gear Gear) Chainring() float64 {
+	return gear.chainring
+}
+
+func (gear Gear) Cog() float64 {
+	return gear.cog
+}
+
+func (gear Gear) Ratio() float64 {
+	return gear.Chainring() / gear.Cog()
+}
+
+func main() {
+	fmt.Println(NewGear(52, 11).Ratio())
+	fmt.Println(NewGear(30, 27).Ratio())
+}
