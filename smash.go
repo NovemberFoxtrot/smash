65db78938ae2dc11a3130ddc49252a727e2ff823
NovemberFoxtrot <november@fox.trot>
2013-09-03 18:33:10 -0700

    playing

--- smash.go
+++ smash.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 type Gear struct {
 	chainring float64
 	cog       float64
+	wheel     *wheel
 }
 
-func NewGear(chainring, cog float64) Gear {
-	return Gear{chainring, cog}
+type wheel struct {
+	rim  float64
+	tire float64
+}
+
+func NewGear(chainring, cog, rim, tire float64) Gear {
+	return Gear{chainring: chainring, cog: cog, wheel: &wheel{rim, tire}}
 }
 
 func (gear Gear) Chainring() float64 {
@@ -21,11 +28,36 @@ func (gear Gear) Cog() float64 {
 	return gear.cog
 }
 
+func (wheel wheel) Rim() float64 {
+	return wheel.rim
+}
+
+func (wheel wheel) Tire() float64 {
+	return wheel.tire
+}
+
+func (wheel wheel) Diameter() float64 {
+	return wheel.Rim() + (wheel.Tire() * 2)
+}
+
+func (wheel wheel) Circumference() float64 {
+	return wheel.Diameter() * math.Pi
+}
+
 func (gear Gear) Ratio() float64 {
 	return gear.Chainring() / gear.Cog()
 }
 
+func (gear Gear) Wheel() wheel {
+	return *gear.wheel
+}
+
+func (gear Gear) GearInches() float64 {
+	return gear.Ratio() * gear.Wheel().Diameter()
+}
+
 func main() {
-	fmt.Println(NewGear(52, 11).Ratio())
-	fmt.Println(NewGear(30, 27).Ratio())
+	fmt.Println(NewGear(52, 11, 26, 1.5).GearInches())
+	fmt.Println(NewGear(52, 11, 24, 1.25).GearInches())
+	fmt.Println(NewGear(52, 11, 24, 1.25).Wheel().Circumference())
 }
